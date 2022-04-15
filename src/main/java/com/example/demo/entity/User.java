6b639dd000eb16440d45a70dcaf1227922ad5fb9
wangyuanrong <wang.yuanrong@qq.com>
2022-04-15 16:55:53 +0800

    基础类

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,55 +1,20 @@
 package com.example.demo.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
-import java.io.Serializable;
-import java.util.Date;
 
 /**
  * 用户
  * @author WangYuanrong
  * @since 2021-06-18
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
-@EqualsAndHashCode(callSuper = false)
 @TableName("tb_user")
 @ApiModel(value="User对象", description="用户")
-public class User implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    @TableId(value = "id", type = IdType.AUTO)
-    private Long id;
-
-    /**
-     * 创建人
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Long createBy;
-
-    /**
-     * 创建日期
-     */
-    private Date createTime;
-
-    /**
-     * 更新人
-     */
-    @TableField(fill = FieldFill.UPDATE)
-    private Long updateBy;
-
-    /**
-     * 更新日期
-     */
-    private Date updateTime;
-
+public class User extends BaseCreatorEntity {
 
     private String openId;
 
